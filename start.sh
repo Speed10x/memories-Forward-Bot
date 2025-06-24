cf9a90ac53289a81da56e91180c079d1e2d1f83b
Hemant Kumar <160343394+Speed10x@users.noreply.github.com>
2025-06-24 19:17:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/Jisshubot/Jisshu-forward-bot Jisshubot/Jisshu-forward-bot 
+  git clone https://github.com/Speed10x/memories-Forward-Bot Speed10x/memories-Forward-Bot 
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/Jisshubot/Jisshu-forward-bot -b $BRANCH /Jisshu-forward-bot
+  git clone https://github.com/Speed10x/memories-Forward-Bot -b $BRANCH /memories-Forward-Bot
 fi
-cd Jisshubot/Jisshu-forward-bot 
+cd Speed10x/memories-Forward-Bot 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 gunicorn app:app & python3 main.py
