2a76546d040902ef7ffa6d4f5a474e02dcc4428a
Hemant Kumar <160343394+Speed10x@users.noreply.github.com>
2025-06-24 19:32:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,20 +3,20 @@ from os import environ
 
 class Config:
     
-    API_ID = environ.get("API_ID", "")
-    API_HASH = environ.get("API_HASH", "")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
+    API_ID = environ.get("API_ID", "23148998")
+    API_HASH = environ.get("API_HASH", "d948f2f1b811b3fa298cd10f9add3331")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7667315051:AAFhe6o6_7VWPjNC2CuFOhBUx7Mw17F28Rs") 
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '5223903292').split()]
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
 
     PICS = (environ.get('PICS', 'https://graph.org/file/e223aea8aca83e99162bb.jpg'))
     
-    DATABASE_URI = environ.get("DATABASE_URI", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://memo:memo@memories.woq9ths.mongodb.net/?retryWrites=true&w=majority&appName=memories")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "memories")
     
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
-    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") # FORCE SUB channel link 
-    FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")  # FORCE SUB ON - OFF
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002227605732'))
+    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "-1002238440461") # FORCE SUB channel link 
+    FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "False")  # FORCE SUB ON - OFF
 
 
 class temp(object): 
